ec97c5e1016e27859da872cb6183013bc1126d08
Patrick Schleizer <adrelanos@riseup.net>
2014-05-23 18:24:34 +0000

    .

---
+++ etc/iceweasel/pref/anon-iceweasel-warning.js
@@ -0,0 +1,5 @@
+// This file is part of Whonix
+// Copyright (C) 2012 - 2014 Patrick Schleizer <adrelanos@riseup.net>
+// See the file COPYING for copying conditions.
+
+pref("browser.startup.homepage","data:text/plain,browser.startup.homepage=/usr/share/anon-iceweasel-warning/iceweasel_local_homepage.html");
